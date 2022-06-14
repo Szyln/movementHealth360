71f0774165cac4c93c489f04ac6bc6b685a162f0
Szyln <szyen.lin@gmail.com>
2022-06-14 23:32:12 +0800

    page headings adjust

--- src/components/page-title-component.jsx
+++ src/components/page-title-component.jsx
@@ -4,12 +4,12 @@ function PageTitleComponent({ children = '頁面標題', id, headingLevel }) {
   return (
     <>
       { headingLevel === 'h2' && (
-        <h2 className="text-primary border-bottom pt-16 pt-sm- pt-md-38 pt-lg-20 mb-20" id={id}>
+        <h2 className="text-primary border-bottom pt-16 pt-sm- pt-md-38 pt-lg-20 mb-20 fs-h3 fs-md-h2" id={id}>
           {children}
         </h2>
       ) }
       {!id && (
-        <h1 className="text-primary border-bottom pt-50 mb-20" id={id}>
+        <h1 className="text-primary border-bottom pt-25 pt-md-50 mb-20 fs-h2 fs-md-h1" id={id}>
           {children}
         </h1>
       )}
