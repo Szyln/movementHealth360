750a5466a56215569b52d87d370d8be9c2e50ee8
Szyln <szyen.lin@gmail.com>
2022-06-17 18:20:27 +0800

    homepage service card design adjust

--- src/components/service-card-homepage-component.jsx
+++ src/components/service-card-homepage-component.jsx
@@ -9,14 +9,14 @@ function ServiceCardHomepageComponent({ service, category }) {
 
   return (
     <div className="border-primary border-around rounded">
-      <div className="round-top bg-primary h-60 p-5 d-flex flex-column justify-content-end align-items-start">
-        <h4 className="fs-h5 mb-3 bg-primary-dark text-white p-2 rounded-sm fw-normal lh-1 d-inline-block lh-1 letter-space-sm">{category.name}</h4>
+      <div className="round-top bg-primary h-45 h-md-60 p-5 d-flex flex-column justify-content-end align-items-start">
+        <h4 className="fs-h5 mb-3 bg-primary-dark text-white p-2 rounded-sm fw-normal d-inline-block lh-1 letter-space-sm">{category.name}</h4>
         <h3 className="text-white fs-h4 letter-space-normal mb-5">
           {name}
         </h3>
       </div>
       <div className="d-flex flex-column justify-content-end align-items-stretch p-5 ">
-        <img src={category.img} alt={category.name} className="h-70" />
+        <img src={category.img} alt={category.name} className="h-60" />
       </div>
 
     </div>
