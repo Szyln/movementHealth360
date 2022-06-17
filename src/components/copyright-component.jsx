94cf5eaeb97506c0a33e45ef30da5eb13dc351a7
Szyln <szyen.lin@gmail.com>
2022-06-17 18:14:52 +0800

    licenses declare

--- src/components/copyright-component.jsx
+++ src/components/copyright-component.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Copyright() {
   return (
-    <small className="text-primary fw-normal text-center d-block fs-small letter-space-sm">© 動作健康 360 2022</small>
+    <ul className="text-primary fw-normal text-center fs-small letter-space-sm">
+      <li><small>© 動作健康 360 2022</small></li>
+      <li><small><a href="https://storyset.com/people">People illustrations by Storyset</a></small></li>
+      <li>
+        <small>
+          チェックポイントフォント design by マルセ様
+          <a href="http://marusexijaxs.web.fc2.com/" className="ms-2">よく飛ばない鳥</a>
+        </small>
+      </li>
+    </ul>
   );
 }
 
