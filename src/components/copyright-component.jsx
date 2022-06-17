bc033c3f14142cebc0844259bd30892cefe84315
Szyln <szyen.lin@gmail.com>
2022-06-17 18:36:50 +0800

    design adjust

--- src/components/copyright-component.jsx
+++ src/components/copyright-component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Copyright() {
   return (
-    <ul className="text-primary fw-normal text-center fs-small letter-space-sm">
+    <ul className="text-primary fw-normal text-center fs-small letter-space-sm mt-5">
       <li><small>© 動作健康 360 2022</small></li>
       <li><small><a href="https://storyset.com/people">People illustrations by Storyset</a></small></li>
       <li>
