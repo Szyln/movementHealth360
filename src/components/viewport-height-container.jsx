c6e2b539a7a09cd09970b01aacb0dfdc2e0801e3
Szyln <szyen.lin@gmail.com>
2022-06-17 18:36:34 +0800

    hidden prop adjust

--- src/components/viewport-height-container.jsx
+++ src/components/viewport-height-container.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 function ViewportHeightContainer({
-  children, bgColor = '', isFluid = false, container = true, fullHeight = true, otherClassName = "" , isHidden = false
+  children, bgColor = '', isFluid = false, container = true, fullHeight = true, otherClassName = '', isHidden = false,
 }) {
-  return (
-    <div
-      className={`${fullHeight ? 'h-100vh' : 'py-10 py-md-20'} ${isHidden ? 'd-none' : 'd-flex'} align-items-center bg-${bgColor} ${otherClassName}`}
-    >
-      <div className="w-100p">
-        <div className={`${container ? '' : 'disable-'}container${isFluid ? '-fluid' : ''}`}>
-          {children}
+  if (isHidden) {
+    return null;
+  } if (!isHidden) {
+    return (
+      <div
+        className={`${fullHeight ? 'h-100vh' : 'py-10 py-md-20'} d-flex align-items-center bg-${bgColor} ${otherClassName}`}
+      >
+        <div className="w-100p">
+          <div className={`${container ? '' : 'disable-'}container${isFluid ? '-fluid' : ''}`}>
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ViewportHeightContainer;
