6431f011747f4683f4e74d25fa9e701e7ead1a94
Szyln <szyen.lin@gmail.com>
2022-06-20 21:24:47 +0800

    product card layout

--- src/pages/service-product-page.jsx
+++ src/pages/service-product-page.jsx
@@ -5,35 +5,36 @@ import { v4 as uuidv4 } from 'uuid';
 import PageTitleComponent from '../components/page-title-component';
 import ServiceCardComponent from '../components/service-card-component';
 
-function ServiceProductPage({ servicesList, filterCategoryProductList, getServiceName, category }) {
+function ServiceProductPage({
+  servicesList, filterCategoryProductList, getServiceName, category,
+}) {
   // get from /service/:category
   // const { category } = useParams();
 
   return (
-    <div className="row">
+    <div className="col-12">
       {/* products (sort by category) */}
       {/* title: category name */}
       <Link to={`/service/#${category}`}>
         <PageTitleComponent id={category} headingLevel="h2">{category}</PageTitleComponent>
       </Link>
-      {/* products in this category */}
-      <div className="col-lg-10 offset-lg-1 mb-6 ">
-        <div className="row">
+      <div className="row">
+        {/* products in this category */}
+        <div className="col-lg-10 offset-lg-1 mb-6">
+          {/* product card */}
           {filterCategoryProductList(category).map((product) => (
-            <div className="col-md-6 mb-4" key={uuidv4()}>
+            <div className="mb-4" key={uuidv4()}>
               <ServiceCardComponent service={product} getServiceName={getServiceName} />
             </div>
           ))}
-
         </div>
-
-      </div>
-      <div className="row">
-        <div className="col-lg-10 offset-lg-1">
-          <Link to="/service" className="text-center d-block py-5">回到頂端</Link>
-
+        <div className="row">
+          <div className="col-lg-10 offset-lg-1">
+            <Link to="/service" className="text-center d-block py-5">回到頂端</Link>
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
